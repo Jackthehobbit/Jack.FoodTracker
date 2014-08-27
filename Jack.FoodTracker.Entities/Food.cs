9e48af1c289c7f6007d86cd3b23072b191dae0ea
Jackthehobbit <scunnyrule45@hotmail.co.uk>
2014-08-27 19:13:33 +0100

    Added missing entity framework dll

---
+++ Jack.FoodTracker.Entities/Food.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jack.FoodTracker.Entities
+{
+    public class Food
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Calories { get; set; }
+    }
+}
