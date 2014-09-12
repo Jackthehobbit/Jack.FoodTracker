f2e81ec6ee248c8738414b79d3c268c81b66fee0
Jackthehobbit <scunnyrule45@hotmail.co.uk>
2014-09-12 22:39:45 +0100

    Added missing Interface file

---
+++ Jack.FoodTracker.EntityDatabase/IFoodCategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Jack.FoodTracker.Entities;
+using System;
+using System.Collections.Generic;
+namespace Jack.FoodTracker.EntityDatabase
+{
+    public interface IFoodCategoryRepository
+    {
+        List<FoodCategory> GetAll();
+    }
+}
