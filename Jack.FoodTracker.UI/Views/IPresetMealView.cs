e40cddf21bcf57320bbe58b54db53259273c8620
Jackthehobbit <scunnyrule45@hotmail.co.uk>
2014-11-26 21:57:32 +0000

    Added IPresetMealView - Empty atm, resolves #34

---
+++ Jack.FoodTracker.UI/Views/IPresetMealView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jack.FoodTracker.Views
+{
+    public interface IPresetMealView
+    {
+
+    }
+}
